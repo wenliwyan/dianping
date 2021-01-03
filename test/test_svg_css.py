ded9f609596a9940794bd15e7ec683d569ae66a3
wenliwyan <12013376+wenliwyan@users.noreply.github.com>
2021-01-03 19:37:12 +0800

    remove trailing whitespace in url
    the tests failed because of the trailing whitespace?
    http response was an XML file with Code: InvalidBucketName

--- test/test_svg_css.py
+++ test/test_svg_css.py
@@ -7,6 +7,6 @@ class TestSvgCss(TestCase):
     def test_get_text(self):
         css_url = 'http://s3plus.meituan.net/v1' \
                   '/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss' \
-                  '/fbf48635df260f2a46cadd5a2d064522.css '
+                  '/fbf48635df260f2a46cadd5a2d064522.css'
         svg_css = SvgCss(css_url)
         self.assertEqual('的', svg_css.get_text('scbifa'))
